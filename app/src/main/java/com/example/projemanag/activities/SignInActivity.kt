84664c482c34a01e7ddbca8233556847f9f6f6ba
Andrii Zubenko <andrei.zubenko@gmail.com>
2020-05-19 15:13:52 -0700

    complete app

---
+++ app/src/main/java/com/example/projemanag/activities/SignInActivity.kt
@@ -0,0 +1,94 @@
+package com.example.projemanag.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.view.WindowManager
+import android.widget.Toast
+import com.example.projemanag.R
+import com.example.projemanag.firebase.FirestoreClass
+import com.example.projemanag.models.User
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_sign_in.*
+
+class SignInActivity : BaseActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private val TAG = "Sign in"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+
+        auth = FirebaseAuth.getInstance()
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        btn_sign_in.setOnClickListener {
+            signInRegisteredUser()
+        }
+
+        setUpActionBar()
+    }
+
+    fun signInSuccess(user: User) {
+        hideProgressDialog()
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+
+    private fun setUpActionBar() {
+        setSupportActionBar(toolbar_sign_in_activity)
+
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_black_color_back_24dp)
+        }
+
+        toolbar_sign_in_activity.setNavigationOnClickListener { onBackPressed() }
+    }
+
+    private fun signInRegisteredUser() {
+        val email: String = et_email_sign_in.text.toString().trim { it <= ' ' }
+        val password: String = et_password_sign_in.text.toString().trim { it <= ' ' }
+
+        if (validateForm(email, password)) {
+            showProgressDialog(resources.getString(R.string.please_wait))
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        FirestoreClass().loadUserData(this@SignInActivity)
+                        Log.d(TAG, "signInWithEmail:success")
+                    } else {
+                        Log.w(TAG, "signInWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            this@SignInActivity,
+                            "Authentication failed.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+        }
+    }
+
+    private fun validateForm(email: String, password: String): Boolean {
+        return when {
+            TextUtils.isEmpty(email) -> {
+                showErrorSnackBar("Please enter an email address")
+                false
+            }
+            TextUtils.isEmpty(password) -> {
+                showErrorSnackBar("Please enter a password")
+                false
+            }
+            else -> {
+                true
+            }
+        }
+    }
+}
