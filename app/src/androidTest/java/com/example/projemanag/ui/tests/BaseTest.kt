197e0b8f492792c01090748844b8623af164693c
Andrii Zubenko <andrei.zubenko@gmail.com>
2020-05-21 18:12:52 -0700

    added orchestrator

--- app/src/androidTest/java/com/example/projemanag/ui/tests/BaseTest.kt
+++ app/src/androidTest/java/com/example/projemanag/ui/tests/BaseTest.kt
@@ -2,28 +2,21 @@ package com.example.projemanag.ui.tests
 
 import androidx.test.espresso.IdlingRegistry
 import com.example.projemanag.utils.Utils
-import com.schibsted.spain.barista.rule.cleardata.ClearDatabaseRule
-import com.schibsted.spain.barista.rule.cleardata.ClearFilesRule
-import com.schibsted.spain.barista.rule.cleardata.ClearPreferencesRule
 import org.junit.After
 import org.junit.Before
-import org.junit.Rule
 
 open class BaseTest {
     @Before
     fun setup() {
         IdlingRegistry.getInstance().register(Utils.countingIdlingResource)
+//        var currentUser = FirebaseAuth.getInstance().currentUser
+//        if (currentUser != null) {
+//            FirebaseAuth.getInstance().signOut()
+//        }
     }
 
     @After
     fun tearDown() {
         IdlingRegistry.getInstance().unregister(Utils.countingIdlingResource)
     }
-
-//    @get:Rule
-//    var clearPreferencesRule = ClearPreferencesRule()
-//    @get:Rule
-//    var clearDatabaseRule = ClearDatabaseRule()
-//    @get:Rule
-//    var clearFilesRule = ClearFilesRule()
 }
--- app/src/androidTest/java/com/example/projemanag/ui/tests/SignInTest.kt
+++ app/src/androidTest/java/com/example/projemanag/ui/tests/SignInTest.kt
@@ -64,5 +64,4 @@ class SignInTest : BaseTest() {
             clickSignInButton()
         }
     }
-
 }
