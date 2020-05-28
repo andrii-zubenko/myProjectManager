4b66ead83b80107e0c2d1401d7567fe8a5307896
Andrii Zubenko <andrei.zubenko@gmail.com>
2020-05-28 09:45:53 -0700

    added assertion to sigUp test

--- app/src/androidTest/java/com/example/projemanag/ui/tests/SignUpTest.kt
+++ app/src/androidTest/java/com/example/projemanag/ui/tests/SignUpTest.kt
@@ -4,12 +4,16 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
 import com.example.projemanag.activities.SplashActivity
+import com.example.projemanag.ui.robots.drawer
 import com.example.projemanag.ui.robots.intro
+import com.example.projemanag.ui.robots.main
 import com.example.projemanag.ui.robots.signUp
 import com.example.projemanag.ui.utils.dateInMills
+import org.junit.Assert
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.lang.AssertionError
 
 @LargeTest
 @RunWith(AndroidJUnit4::class)
@@ -53,6 +57,14 @@ class SignUpTest : BaseTest() {
             typeInPassword(validPassword)
             tapOnSignUpButton()
             toastWithTextIsDiplayed(toastRegSuccess)
+        }
+
+        main {
+            openMemberMenuDrawer()
+        }
+
+        drawer {
+            Assert.assertEquals(validName, getUserName())
         }
     }
 }
